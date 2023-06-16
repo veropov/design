9db2bdd7959d7d56d6350bad5b7bc5fc2303fbee
v.eropov <vyacheslav.eropov@yandex.ru>
2023-06-16 09:35:37 +0800

    update yand animation

--- src/yandex/yandex-slider.jsx
+++ src/yandex/yandex-slider.jsx
@@ -8,7 +8,22 @@ import whiteMusic from './style/Lottie/slider/white-music.lottie';
 import blueButt from './style/Lottie/slider/blue-button.lottie';
 import blueMusic from './style/Lottie/slider/blue-music.lottie';
 
+import blacB from './style/Lottie/slider/new/bb.lottie';
+import blacM from './style/Lottie/slider/new/bm.lottie';
+import RedB from './style/Lottie/slider/new/rb.lottie';
+import RedM from './style/Lottie/slider/new/rm.lottie';
+import BlueB from './style/Lottie/slider/new/blb.lottie';
+import BlueM from './style/Lottie/slider/new/blm.lottie';
+import WhiteB from './style/Lottie/slider/new/wb.lottie';
+import WhiteM from './style/Lottie/slider/new/wm.lottie';
+
+import blackTrans from './style/Lottie/slider/trans/black-trans-slider.lottie';
+import redTrans from './style/Lottie/slider/trans/red-trans-slider.lottie';
+import blueTrans from './style/Lottie/slider/trans/blue-trans-slider.lottie';
+import whiteTrans from './style/Lottie/slider/trans/white-trans-slider.lottie';
+
 import '@dotlottie/player-component';
+import Lottie from "lottie-react";
 
 import black from './style/assets/black.jpg';
 import blue from './style/assets/blue.jpg';
@@ -22,40 +37,65 @@ export const YandSlider = () => {
         active: null,
         obj: [{id: 1}, {id: 2}, {id: 3}, {id: 4}]
     })
+
+    const [trans, setTrans] = useState({
+        active: null,
+        disActive: null,
+        obj: [{id: 1}, {id: 2}, {id: 3}, {id: 4}]
+    })
     
     const sliderTrans = [
-        blackMusic,
-        redMusic,
-        blueMusic,
-        whiteMusic
+        redTrans,
+        blackTrans,
+        blueTrans,
+        whiteTrans
     ]
 
     const clickButt = [
-        blackButt,
-        redButt,
-        blueButt,
-        whiteButt
+        RedB,
+        blacB,
+        BlueB,
+        WhiteB,
     ]
 
     const activeButt = [
-        blackMusic,
-        redMusic,
-        blueMusic,
-        whiteMusic
+        RedM,
+        blacM,
+        BlueM,
+        WhiteM,
     ]
 
     const buttObj = [
-        (<div className="butt_yand_slider_black"></div>),
         (<div className="butt_yand_slider_red"></div>),
+        (<div className="butt_yand_slider_black"></div>),
         (<div className="butt_yand_slider_blue"></div>),
         (<div className="butt_yand_slider_white"></div>)
     ]
 
     const yandImg = [
-        black,
         red,
+        black,
         blue,
         white
+    ]
+
+    function redText() {
+        return (
+            <div className="slider_yand_text">
+                <p className="slider_yand_red_limit">Лимитированная серия</p>
+                <p className="slider_yand_p_light">Яндекс Станция драгоценного рубинового цвета станет <br />акцентом в вашем интерьере</p>
+                <div className="slider_yand_red_button">
+                    <p>Купить</p>
+                </div>
+            </div>
+        )
+    }
+
+    const sliderText = [
+        redText(),
+        redText(),
+        redText(),
+        redText()
     ]
 
     const LottieTrans = (index) => {
@@ -64,22 +104,59 @@ export const YandSlider = () => {
                 <dotlottie-player
                     src={sliderTrans[index]}
                     autoplay
-                    speed={1}
+                    speed={1.4}
                 />
             </div>
         )
     }
 
-    function ToggleImg(index) {
-        if (butt.obj[index] === butt.active) {
-            return yandImg[index]
-        } else return null;
+
+    function ToggleImg() {
+        if (butt.obj[0] === butt.active) {
+            return (
+                <div className="slider_yand_img">
+                    <img src={yandImg[0]} style={{zIndex: 1}}/>
+                    <img src={yandImg[1]} style={{zIndex: 0}}/>
+                    <img src={yandImg[2]} style={{zIndex: 0}}/>
+                    <img src={yandImg[3]} style={{zIndex: 0}}/>
+                </div>
+            )
+        } else if (butt.obj[1] === butt.active) {
+            return (
+                <div className="slider_yand_img">
+                    <img src={yandImg[0]} style={{zIndex: 0}}/>
+                    <img src={yandImg[1]} style={{zIndex: 1}}/>
+                    <img src={yandImg[2]} style={{zIndex: 0}}/>
+                    <img src={yandImg[3]} style={{zIndex: 0}}/>
+                </div>
+            )
+        } else if (butt.obj[2] === butt.active) {
+            return (
+                <div className="slider_yand_img">
+                    <img src={yandImg[0]} style={{zIndex: 0}}/>
+                    <img src={yandImg[1]} style={{zIndex: 0}}/>
+                    <img src={yandImg[2]} style={{zIndex: 1}}/>
+                    <img src={yandImg[3]} style={{zIndex: 0}}/>
+                </div>
+            )
+        } else if (butt.obj[3] === butt.active) {
+            return (
+                <div className="slider_yand_img">
+                    <img src={yandImg[0]} style={{zIndex: 0}}/>
+                    <img src={yandImg[1]} style={{zIndex: 0}}/>
+                    <img src={yandImg[2]} style={{zIndex: 0}}/>
+                    <img src={yandImg[3]} style={{zIndex: 1}}/>
+                </div>
+            )
+        }
     }
 
     function ToggleTrans(index) {
-        if (butt.obj[index] === butt.click) {
+        if (trans.obj[index] === trans.active) {
             return LottieTrans(index)
-        } else return null;
+        } else if (trans.obj[index] === trans.disActive) { 
+            return null;
+        }
     }
 
     const YandSliderData = (index) => {
@@ -88,8 +165,11 @@ export const YandSlider = () => {
                 <div className="slider_yand_trans">
                     {ToggleTrans(index)}
                 </div>
+                <div className="slider_yand_text_cont">
+                    {sliderText[index]}
+                </div>
                 <div className="slider_yand_img">
-                    <img src={ToggleImg(index)}/>
+                    {ToggleImg()}
                 </div>
             </div>
         )
@@ -120,11 +200,21 @@ export const YandSlider = () => {
         )
     }
 
+    function ActiveTrans(index) {
+        return setTrans({...trans, active: trans.obj[index]})
+    }
+
+    function DisActiveTrans(index) {
+        return setTimeout(() => {
+            setTrans({...trans, disActive: trans.obj[index], active: null})
+        }, 900)
+    }
+
 
     function ActiveB(index) {
         return setTimeout(() => {
             setButt({...butt, active: butt.obj[index], click: null})
-        }, 450) 
+        }, 440) 
     }
 
     function ClickB(index) {
@@ -161,6 +251,8 @@ export const YandSlider = () => {
                         onClick={() => {
                             ActiveB(index)
                             ClickB(index)
+                            ActiveTrans(index)
+                            DisActiveTrans(index)
                         }}
                     >
                         {ToggleAnim(index)}
