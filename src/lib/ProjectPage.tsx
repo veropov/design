cdca76dea1de8c1a72144a696af96d6190dac50d
v.eropov <vyacheslav.eropov@yandex.ru>
2023-07-28 09:32:37 +0800

    update app

--- src/lib/ProjectPage.tsx
+++ src/lib/ProjectPage.tsx
@@ -30,7 +30,7 @@ export default function ProjectPage() {
                         <p className='title_cont'>Точечные задачи</p>
                         <div className="project_container">
                             { MainProject('FUN SUN UserFlow', 'Построение user flow анкета-тур (переход через внешние источники)', 'comm', true, true, true, true, false, true, 'https://www.figma.com/file/QtkWSTG7IlYYi2RLYkswZm/Untitled?type=design&node-id=0%3A1&mode=design&t=DVconLdP3coBdlOX-1', Smart, false) }
-                            { MainProject('FUN&SUN Бронирование', 'Оформление услуг в аэропорту, форма обратной связи', 'comm', true, true, true, false, true, false, 'https://www.figma.com/file/SQvge574qM6tTgjtcWM1GJ/FUN%26SUN-projects?type=design&node-id=0%3A1&t=1YttjT2b5n2LNMkz-1', Baggage, false) }
+                            { MainProject('FUN&SUN Бронь', 'Оформление услуг в аэропорту, форма обратной связи', 'comm', true, true, true, false, true, false, 'https://www.figma.com/file/SQvge574qM6tTgjtcWM1GJ/FUN%26SUN-projects?type=design&node-id=0%3A1&t=1YttjT2b5n2LNMkz-1', Baggage, false) }
                             { MainProject('Дилерский сайт Lada', 'Создание продающего лендинга с карточками товаров', 'pet', false, false, true, false, true, false, 'https://www.figma.com/file/k4IDaZbkVdatVAKUXZ4jEc/Lada?type=design&node-id=0%3A1&t=4Tl82BB3qCw5vNn6-1', Fitnest, false) }
                         </div>
                     </div>
